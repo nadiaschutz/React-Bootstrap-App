ace61a8346fc51a48e683444a72002ffae2ede41
wuno <ndiaz@wuno.com>
2017-04-23 01:30:38 -0500

    Added react-router to project

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 class Header extends Component {
 
@@ -6,6 +7,13 @@ class Header extends Component {
     return (
       <div className="Header">
           <h2>HEY THIS IS THE HEADER COMPONENT</h2>
+          <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/services">Services</Link></li>
+              <li><Link to="/portfolio">Portfolio</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
+          </ul>
       </div>
     );
   }
