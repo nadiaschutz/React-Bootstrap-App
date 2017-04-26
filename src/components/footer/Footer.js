5da1213044f28a8d4fa511898fe3ef3d3ace1e80
wuno <ndiaz@wuno.com>
2017-04-26 10:41:17 -0500

    Updated layout of the Home component using Bootstrap v4 / add handling of search field input on submit in Header component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import './Footer.css';
 
 class Footer extends Component {
 
   render() {
     return (
       <div>
-        <footer className="footer">
+        <footer className="footer bg-inverse">
             <div className="container">
-                <span className="text-muted">Place sticky footer content here.</span>
+                <p className="text-muted">Place sticky footer content here.</p>
             </div>
         </footer>
       </div>
--- src/containers/home/Home.js
+++ src/containers/home/Home.js
@@ -4,9 +4,118 @@ class Home extends Component {
 
   render() {
     return (
-      <div>
-          <h2>HEY THIS IS THE HOME PAGE</h2>
-      </div>
+      <div className="container">
+
+        <div className="row">
+            <div className="col-sm-12 col-md-4 col-lg-4">
+                <div className="card">
+                  <div className="card-block">
+                    <h4 className="card-title">Card title</h4>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  </div>
+                  <ul className="list-group list-group-flush">
+                    <li className="list-group-item">Cras justo odio</li>
+                    <li className="list-group-item">Dapibus ac facilisis in</li>
+                  </ul>
+                  <div className="card-block">
+                    <a href="#" className="card-link">Card link</a>
+                    <a href="#" className="card-link">Another link</a>
+                  </div>
+                </div>
+            </div>
+            <div className="col-sm-12 col-md-8 col-lg-8">
+                <div className="card">
+                  <div className="card-block">
+                    <h4 className="card-title">Card title</h4>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  </div>
+                  <ul className="list-group list-group-flush">
+                    <li className="list-group-item">Cras justo odio</li>
+                    <li className="list-group-item">Dapibus ac facilisis in</li>
+                    <li className="list-group-item">Vestibulum at eros</li>
+                  </ul>
+                  <div className="card-block">
+                    <a href="#" className="card-link">Card link</a>
+                    <a href="#" className="card-link">Another link</a>
+                  </div>
+                </div>
+            </div>
+        </div>
+
+        <div className="row">
+          <div className="col-sm-12 col-md-6 col-lg-3">
+              <div className="card">
+                <div className="card-block">
+                  <h4 className="card-title">Card title</h4>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item">Cras justo odio</li>
+                  <li className="list-group-item">Dapibus ac facilisis in</li>
+                  <li className="list-group-item">Vestibulum at eros</li>
+                </ul>
+                <div className="card-block">
+                  <a href="#" className="card-link">Card link</a>
+                  <a href="#" className="card-link">Another link</a>
+                </div>
+              </div>
+          </div>
+          <div className="col-sm-12 col-md-6 col-lg-3">
+              <div className="card">
+                <div className="card-block">
+                  <h4 className="card-title">Card title</h4>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item">Cras justo odio</li>
+                  <li className="list-group-item">Dapibus ac facilisis in</li>
+                  <li className="list-group-item">Vestibulum at eros</li>
+                </ul>
+                <div className="card-block">
+                  <a href="#" className="card-link">Card link</a>
+                  <a href="#" className="card-link">Another link</a>
+                </div>
+              </div>
+          </div>
+
+          <div className="clearfix hidden-sm-up"></div>
+
+          <div className="col-sm-12 col-md-6 col-lg-3">
+              <div className="card">
+                <div className="card-block">
+                  <h4 className="card-title">Card title</h4>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item">Cras justo odio</li>
+                  <li className="list-group-item">Dapibus ac facilisis in</li>
+                  <li className="list-group-item">Vestibulum at eros</li>
+                </ul>
+                <div className="card-block">
+                  <a href="#" className="card-link">Card link</a>
+                  <a href="#" className="card-link">Another link</a>
+                </div>
+              </div>
+          </div>
+          <div className="col-sm-12 col-md-6 col-lg-3">
+              <div className="card">
+                <div className="card-block">
+                  <h4 className="card-title">Card title</h4>
+                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item">Cras justo odio</li>
+                  <li className="list-group-item">Dapibus ac facilisis in</li>
+                  <li className="list-group-item">Vestibulum at eros</li>
+                </ul>
+                <div className="card-block">
+                  <a href="#" className="card-link">Card link</a>
+                  <a href="#" className="card-link">Another link</a>
+                </div>
+              </div>
+          </div>
+        </div>
+    </div>
     );
   }
 }
