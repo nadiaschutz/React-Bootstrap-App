62fe47016e58121a5b1832b044411ce423c0d8fb
wuno <ndiaz@wuno.com>
2017-04-26 19:14:17 -0500

    Removed /utils/History / Updated Header Component Unit Tests

--- src/components/header/Header.test.js
+++ src/components/header/Header.test.js
@@ -6,3 +6,7 @@ it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Header />, div);
 });
+
+it('alerts a String to from the browser', () => {
+  alert('This is a string');
+});
